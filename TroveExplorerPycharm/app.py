2e89969bf68a8b332e77defc8b1d85ccf8727993
Max <maxbrown0101@gmail.com>
2016-01-16 02:38:14 +1300

    completely merged the topic and intersection methods/resources so that now the only resources are /trove/ and /topic/. You can search for trove articles that are related to a set of given wikipedia articles by entering your terms joined by the ampersand (&) character when using the topic resource. e.g. : bengelkite/pagekite.me/topic/james cook&north island

--- TroveExplorerPycharm/app.py
+++ TroveExplorerPycharm/app.py
@@ -9,7 +9,8 @@ api = Api(app)
 TROVE_ARTICLES_PATH = "C:\\!2015SCHOLARSHIPSTUFF\\TroveJSON\\"
 WIKIPEDIA_ARTICLES_PATH = "C:\\!2015SCHOLARSHIPSTUFF\\WikipediaJSON\\"
 VERBOSE_WIKIPEDIA_PATH = "C:\\!2015SCHOLARSHIPSTUFF\\verboseJSON\\"
-APP_BASE_URL = "http://bengelkite.pagekite.me"
+#APP_BASE_URL = "http://bengelkite.pagekite.me"
+APP_BASE_URL = "http://127.0.0.1"
 
 
 class Home(Resource):
@@ -19,7 +20,7 @@ class Home(Resource):
             'search for all trove articles related to a wikipedia title and display their IDs': 'http://127.0.0.1/wikipedia/<some title here>',
             'search for a particular trove article and display the titles of related wikipedia articles': 'http://127.0.0.1/trove/<some trove ID here>'
         }
-#TODO: check encoding that I am using to write to files in java... (some characters are being lost) e.g. Maori
+#TODO: check encoding that I am using to write to files in java... (some characters are being lost) e.g. Maori a grave/acute symbol thins was getting lost in my google docs dump
 
 class TroveArticle(Resource):
     def get(self, troveID):
@@ -27,43 +28,28 @@ class TroveArticle(Resource):
         with open(filePath, "r") as troveFile:
             troveArticle = json.load(troveFile)
             #we need to build a list of dictionaries where each entry has a title and a url
-            relatedArticles = []
+            relatedTopics = []
             for eachTitle in troveArticle["relatedTopics"]:
                 eachTitleURL = '{}/topic/{}'.format(APP_BASE_URL, eachTitle.replace(" ", "_"))
-                relatedArticles.append({"title" : eachTitle, "topicURL" : eachTitleURL})
-        troveArticle["relatedTopics"] = relatedArticles
+                relatedTopics.append({"title" : eachTitle, "topicURL" : eachTitleURL})
+        troveArticle["relatedTopics"] = relatedTopics
         return troveArticle
 
 
-class Topic(Resource):
-    def get(self, title):
-        with open(WIKIPEDIA_ARTICLES_PATH + title.replace(" ", "_") + ".json") as wikipediaJSON:
-            #fill out a dictionary to return as json
-            wikipediaJSONObj = json.load(wikipediaJSON)
-            result = {}
-            result['topicID'] = (wikipediaJSONObj['topicID'])
-            result['title'] = (wikipediaJSONObj['title'])
-            #now get the addtional info for each related trove article whose info we put under each trove entry
-            relatedTroveArticlesInfo = []
-            for eachTroveID in wikipediaJSONObj["relatedTroveArticles"]:
-                troveArticle = TroveArticle().get(str(eachTroveID))
-                troveArticle['troveURL'] = "{}/trove/{}".format(APP_BASE_URL, eachTroveID)
-                relatedTroveArticlesInfo.append(troveArticle)#TODO: should just have a module level function that does this as a helper and is used by the two resource methods
-            result['articles'] = relatedTroveArticlesInfo
-            return result
 
-class WikipediaArticleIntersection(Resource):
+class Topic(Resource):
   def get(self, titles):
-      listOfSearchTerms = titles.split("&")
-      setsOfIDs = []
-      for each in listOfSearchTerms:
-        eachTopicRelatedArticles = Topic().get(each)["articles"]
-        eachTopicRelatedArticlesIDs = set()
-        for eachArticle in eachTopicRelatedArticles:
-            eachTopicRelatedArticlesIDs.add(eachArticle["uid"])
-        setsOfIDs.append(eachTopicRelatedArticlesIDs)
-      #so now we have an array where each entry is a set of trove UIDs. We need to find their intersection
       result = {}
+      setsOfIDs = []
+      searchedTopics = []
+      listOfSearchTerms = titles.split("&")
+      for eachTerm in listOfSearchTerms:
+        with open("{}{}.json".format(WIKIPEDIA_ARTICLES_PATH, eachTerm.replace(" ", "_"))) as eachTopicJSONFile:
+            eachTopicJSON = json.load(eachTopicJSONFile)
+        searchedTopics.append({"title" : eachTopicJSON["title"], "topicID" : eachTopicJSON["topicID"], "URL" : "{}/topic/{}".format(APP_BASE_URL, eachTopicJSON["title"].replace(" ", "_"))})
+        setsOfIDs.append(set(eachTopicJSON["relatedTroveArticles"]))
+      result["searchedTopics"] = searchedTopics
+      #so now we have an array where each entry is a set of trove UIDs. We need to find their intersection
       intersectionIDs = set.intersection(*setsOfIDs)
       intersectionArticles = []
       for eachTroveID in intersectionIDs:
@@ -71,8 +57,6 @@ class WikipediaArticleIntersection(Resource):
            troveArticle['troveURL'] = "{}/trove/{}".format(APP_BASE_URL, eachTroveID)
            intersectionArticles.append(troveArticle)
       result['articles'] = intersectionArticles
-      #now we have the related trove articles, fill in the rest of the result
-      result['searchedTopics'] = listOfSearchTerms #TODO: this should be a list of dictionaries where each entry has the title, id and a link to that topic's individual page
       return result
 
 
@@ -80,24 +64,13 @@ class WikipediaArticleIntersection(Resource):
 
 
 
-#TODO: consider removing this. just support verbose and trove
-# class WikipediaArticle(Resource):
-#     def get(self, wikipediaTitle):
-#         filePath = "{}{}.json".format(WIKIPEDIA_ARTICLES_PATH, wikipediaTitle.replace(" ","_"))
-#         with open(filePath, "r") as wikipediaFile:
-#             wikipediaArticle = json.load(wikipediaFile)
-#         return wikipediaArticle
-
-
 api.add_resource(Home, '/')
 api.add_resource(TroveArticle, '/trove/<troveID>')
-api.add_resource(Topic, '/topic/<title>')
-api.add_resource(WikipediaArticleIntersection, '/intersection/<titles>')
-#api.add_resource(WikipediaArticle, '/wikipedia/<wikipediaTitle>')
+api.add_resource(Topic, '/topic/<titles>')
+
 
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int("80"), debug=True)
 
-   #print(WikipediaArticleIntersection().get("new zealand&auckland"))
