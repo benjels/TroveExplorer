39fb85ddb31ace1f025097d6ebf9f41aa7f56347
Max <maxbrown0101@gmail.com>
2016-01-21 02:49:08 +1300

    added a couple of flask functions that use jinja html templates to return some ugly html results

--- TroveExplorerPycharm/app.py
+++ TroveExplorerPycharm/app.py
@@ -1,24 +1,34 @@
-#TODO:UNRELATED TO THIS PROGRAM check encoding that I am using to write to files in java... (some characters are being lost) e.g. Maori a grave/acute symbol thins was getting lost in my google docs dump
+
 from flask import Flask
+from flask import render_template
 from flask_restful import Resource, Api
 import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return("there's nobody here")
+
+#TODO: It is a good idea to do as much processing in here rather than in the jinja template right?
+@app.route('/topic/<title>')
+def queryTopics(title):
+    return render_template('topic.html', topics = Topic().get(title)["searchedTopics"], troves = Topic().get(title)["articles"])
+
+@app.route('/trove/<troveID>')
+def queryTrove(troveID):
+    return render_template('trove.html', articleTitle = TroveArticle().get(troveID)["title"], relatedTopics = TroveArticle().get(troveID)["relatedTopics"], fulltext = TroveArticle().get(troveID)["fulltext"])
+
+
+########################################## API ###############################################################
 
 
 
 TROVE_ARTICLES_PATH = "C:\\!2015SCHOLARSHIPSTUFF\\TroveJSON\\"
 WIKIPEDIA_ARTICLES_PATH = "C:\\!2015SCHOLARSHIPSTUFF\\WikipediaJSON\\"
 VERBOSE_WIKIPEDIA_PATH = "C:\\!2015SCHOLARSHIPSTUFF\\verboseJSON\\"
-#APP_BASE_URL = "http://bengelkite.pagekite.me"
-APP_BASE_URL = "http://127.0.0.1"
-
-
-
-class Home(Resource):
-    def get(self):
-        return {
-            'search for all trove articles related to a wikipedia title and display their IDs': 'http://127.0.0.1/topic/<some title here>',
-            'search for a particular trove article and display the titles of related wikipedia articles': 'http://127.0.0.1/trove/<some trove ID here>'
-        }
+#API_BASE_URL = "http://bengelkite.pagekite.me//api"
+API_BASE_URL = "http://127.0.0.1/api"
 
 
 class TroveArticle(Resource):
@@ -29,7 +39,7 @@ class TroveArticle(Resource):
             #we need to build a list of dictionaries where each entry has a title and a url
             relatedTopics = []
             for eachTitle in troveArticle["relatedTopics"]:
-                eachTitleURL = '{}/topic/{}'.format(APP_BASE_URL, eachTitle.replace(" ", "_"))
+                eachTitleURL = '{}/topic/{}'.format(API_BASE_URL, eachTitle.replace(" ", "_"))
                 relatedTopics.append({"title" : eachTitle, "topicURL" : eachTitleURL})
         troveArticle["relatedTopics"] = relatedTopics
         return troveArticle
@@ -45,7 +55,7 @@ class Topic(Resource):
       for eachTerm in listOfSearchTerms:
         with open("{}{}.json".format(WIKIPEDIA_ARTICLES_PATH, eachTerm.replace(" ", "_"))) as eachTopicJSONFile:
             eachTopicJSON = json.load(eachTopicJSONFile)
-        searchedTopics.append({"title" : eachTopicJSON["title"], "topicID" : eachTopicJSON["topicID"], "URL" : "{}/topic/{}".format(APP_BASE_URL, eachTopicJSON["title"].replace(" ", "_"))})
+        searchedTopics.append({"title" : eachTopicJSON["title"], "topicID" : eachTopicJSON["topicID"], "URL" : "{}/topic/{}".format(API_BASE_URL, eachTopicJSON["title"].replace(" ", "_"))})
         setsOfIDs.append(set(eachTopicJSON["relatedTroveArticles"]))
       result["searchedTopics"] = searchedTopics
       #so now we have an array where each entry is a set of trove UIDs. We need to find their intersection and then make JSON dictionaries for each trove article of interest.
@@ -53,17 +63,15 @@ class Topic(Resource):
       intersectionArticles = []
       for eachTroveID in intersectionIDs:
            troveArticle = TroveArticle().get(str(eachTroveID))
-           troveArticle['troveURL'] = "{}/trove/{}".format(APP_BASE_URL, eachTroveID)
+           troveArticle['troveURL'] = "{}/trove/{}".format(API_BASE_URL, eachTroveID)
            intersectionArticles.append(troveArticle)
       result['articles'] = intersectionArticles
       return result
 
 
-app = Flask(__name__)
 api = Api(app)
-api.add_resource(Home, '/')
-api.add_resource(TroveArticle, '/trove/<troveID>')
-api.add_resource(Topic, '/topic/<titles>')
+api.add_resource(TroveArticle, '/api/trove/<troveID>')
+api.add_resource(Topic, '/api/topic/<titles>')
 
 
 if __name__ == '__main__':
